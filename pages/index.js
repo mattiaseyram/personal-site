2ab041647aa1b59eda23bd7349a108ef5e84a770
Mattias Eyram <mattiaseyram@gmail.com>
2019-04-23 11:15:20 -0400

    Test 1

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import content from '../content/home.md';
 
-export default class Home extends Component {
-  render() {
-    let { html , attributes:{ title, cats } } = content;
+export default function Home() {
+
+    console.log(content)
+    const [count, setCount] = useState(0);
+
     return (
-      <article>
-          <h1>{title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: html }}/>
-          <ul>
-              { cats.map((cat, k) => (
-                  <li key={k}>
-                    <h2>{cat.name}</h2>
-                    <p>{cat.description}</p>
-                  </li>
-              ))}
-          </ul>
-      </article>
-    )
-  }
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count + 1)}>
+            Click me
+          </button>
+        </div>
+      );
 }
